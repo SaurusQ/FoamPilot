d712f98e0348d98b506e9b3e2fba1065bb67bb48
Saurus <313.saurus@gmail.com>
2020-03-06 23:42:05 +0200

    Added basic implementation mpl altitude sensor

--- Hardware/Sensor/Altimeter/mpl3115a2.cpp
+++ Hardware/Sensor/Altimeter/mpl3115a2.cpp
@@ -1,2 +1,42 @@
 
 #include "mpl3115a2.hpp"
+
+Mpl3115a2::Mpl3115a2()
+    : Altimeter()
+    , pI2cBus_(I2c_t::getInstance())
+{
+    this->init();
+}
+
+void Mpl3115a2::init()
+{
+    pI2cBus_->writeByte(I2C_ADDR, CTRL_REG1, 0xB8); // set OSR = 128
+    pI2cBus_->writeByte(I2C_ADDR, PT_DATA_CFG, 0x07); // set data ready event
+    pI2cBus_->writeByte(I2C_ADDR, CTRL_REG1, 0xB9); // set active
+}
+
+void Mpl3115a2::reset()
+{
+    pI2cBus_->writeByte(I2C_ADDR, CTRL_REG1, 0x04); // reset bit
+    this->init();
+}
+
+void Mpl3115a2::update()
+{
+    // simple temp read
+    // check for ready data
+    uint8_t STA;
+    do
+    {
+        STA = pI2cBus_->readByte(I2C_ADDR, STATUS);
+    } while(!(STA & 0x08)); // is data ready
+
+    uint8_t msb = pI2cBus_->readByte(I2C_ADDR, OUT_P_MSB);
+    uint8_t csb = pI2cBus_->readByte(I2C_ADDR, OUT_P_CSB);
+    uint8_t lsb = pI2cBus_->readByte(I2C_ADDR, OUT_P_LSB);
+
+    altitude_ = 
+        ((msb & ~(0x01 << 7)) << 12) |
+        (csb << 4) |
+        (lsb & 0x0F);
+}
--- Hardware/Sensor/Altimeter/mpl3115a2.hpp
+++ Hardware/Sensor/Altimeter/mpl3115a2.hpp
@@ -1,19 +1,36 @@
 
-#include "hwSelect.hpp"
-
-#ifdef MPL3115A2
 #ifndef MPL3115A2_HPP
 #define MPL3115A2_HPP
+#include "hwSelect.hpp"
+#ifdef MPL3115A2
 
-class Mpl3115a2
+#include "altimeter.hpp"
+#include "I2cBus.hpp"
+
+#define I2C_ADDR 0x60
+
+#define STATUS      0x00
+#define OUT_P_MSB   0x01
+#define OUT_P_CSB   0x02
+#define OUT_P_LSB   0x03
+#define PT_DATA_CFG 0x13
+#define CTRL_REG1   0x26
+
+class Mpl3115a2 : public Altimeter
 {
     public:
+        Mpl3115a2();
+        virtual ~Mpl3115a2() {}
 
+        virtual void init();
+        virtual void reset();
+        virtual void calibrate() {}
+        virtual void update();
     protected:
-
+        I2cBus* pI2cBus_;
 };
 
-typedef Mpl3115a2 Altimeter_t
+typedef Mpl3115a2 Altimeter_t;
 
 #endif
 #endif
